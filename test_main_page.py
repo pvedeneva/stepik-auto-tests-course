28e5fcf2fcd2a5d9f1f7a7f57479478e3f1e3a7a
pvedenyova <Moisey_90>
2021-12-04 22:57:59 +0700

    add pages, base class and open method

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,11 @@
 import pytest
 from selenium import webdriver
 
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
+def go_to_login_page(browser):
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    go_to_login_page(browser) 
