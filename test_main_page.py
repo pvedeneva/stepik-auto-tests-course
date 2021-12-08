3f3b5806b414a3e3d13d89cc982979d528ba3bdb
pvedenyova <Moisey_90>
2021-12-08 07:52:15 +0700

    check login link

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,10 @@
 import pytest
 from selenium import webdriver
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser) 
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page.should_be_login_link()
